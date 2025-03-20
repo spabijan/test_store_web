f1b3126048642353c6f6e763be6e205d252b2c02
spabijan <pabi@toya.net.pl>
2025-03-20 16:32:15 +0100

    fixes

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -2,6 +2,6 @@ final class GlobalVariables {
   GlobalVariables._();
   static String uri = 'http://localhost:3000';
   static const Map<String, String> headers = {
-    'Content-Type': 'application/json;  charset=UTF-8'
+    'Content-Type': 'application/json;  charset=UTF-8',
   };
 }
